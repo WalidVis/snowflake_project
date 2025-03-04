8d7662c137be48b14d71920b90752962daa833ed
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-04 15:57:54 +0100

    Correct user resources name

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -9,7 +9,7 @@ resource "snowflake_grant_account_role" "rolegrant" {
   parent_role_name = "SYSADMIN"
 }
 
-resource "snowflake_user" "user" {
+resource "snowflake_user" "user1" {
     provider          = snowflake.security_admin
     name              = "walid_cheriet"
     default_warehouse = snowflake_warehouse.warehouse.name
@@ -18,7 +18,7 @@ resource "snowflake_user" "user" {
    // rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
 
-resource "snowflake_user" "user" {
+resource "snowflake_user" "user2" {
     provider          = snowflake.security_admin
     name              = "saif_bouaziz"
     default_warehouse = snowflake_warehouse.warehouse.name
@@ -27,7 +27,7 @@ resource "snowflake_user" "user" {
    // rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
 
-resource "snowflake_user" "user" {
+resource "snowflake_user" "user3" {
     provider          = snowflake.security_admin
     name              = "zied_maalej"
     default_warehouse = snowflake_warehouse.warehouse.name
@@ -36,7 +36,7 @@ resource "snowflake_user" "user" {
    // rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
 
-resource "snowflake_user" "user" {
+resource "snowflake_user" "user4" {
     provider          = snowflake.security_admin
     name              = "issam_eddaou"
     default_warehouse = snowflake_warehouse.warehouse.name
@@ -45,7 +45,7 @@ resource "snowflake_user" "user" {
    // rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
 
-resource "snowflake_user" "user" {
+resource "snowflake_user" "user5" {
     provider          = snowflake.security_admin
     name              = "rihab_bahri"
     default_warehouse = snowflake_warehouse.warehouse.name
@@ -54,7 +54,7 @@ resource "snowflake_user" "user" {
    // rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
 
-resource "snowflake_user" "user" {
+resource "snowflake_user" "user6" {
     provider          = snowflake.security_admin
     name              = "matthieu_noiret"
     default_warehouse = snowflake_warehouse.warehouse.name
@@ -63,7 +63,7 @@ resource "snowflake_user" "user" {
    // rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
 
-resource "snowflake_user" "user" {
+resource "snowflake_user" "user7" {
     provider          = snowflake.security_admin
     name              = "amine_snoussi"
     default_warehouse = snowflake_warehouse.warehouse.name
