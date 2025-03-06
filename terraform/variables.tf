ff38da903494db4876ca901b5d38ef2763991e72
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-06 16:42:16 +0100

    Clean variable.fr file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,15 +9,3 @@ variable "warehouse_size" {
   type        = string
   description = "warehouse size"
 }
-
-variable "SNOWFLAKE_ACCOUNT_NAME" {
-  type        = string
-  description = "warehouse size"
-  default = ""
-}
-
-variable "SNOWFLAKE_ORGANIZATION_NAME" {
-  type        = string
-  description = "warehouse size"
-  default = ""
-}
