25e3fe1ffd33ca92890102c0cfa184e95a65cdd0
Matthieu Noiret <matthieu.noiret@viseo.com>
2025-03-10 16:46:18 +0100

    Updated grant.tf to grant role to all previously created users

--- terraform/grants.tf
+++ terraform/grants.tf
@@ -1,5 +1,41 @@
-resource "snowflake_grant_account_role" "grant_poc_viseo_role" {
+resource "snowflake_grant_account_role" "grant_poc_viseo_role_sbouaziz" {
   provider  = snowflake.security_admin
   role_name = snowflake_account_role.role.name
   user_name = snowflake_user.saif_bouaziz.name
+}
+
+resource "snowflake_grant_account_role" "grant_poc_viseo_role_wcheriet" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_account_role.role.name
+  user_name = snowflake_user.walid_cheriet.name
+}
+
+resource "snowflake_grant_account_role" "grant_poc_viseo_role_zmaalej" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_account_role.role.name
+  user_name = snowflake_user.zied_maalej.name
+}
+
+resource "snowflake_grant_account_role" "grant_poc_viseo_role_ieddaou" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_account_role.role.name
+  user_name = snowflake_user.issam_eddaou.name
+}
+
+resource "snowflake_grant_account_role" "grant_poc_viseo_role_rbahri" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_account_role.role.name
+  user_name = snowflake_user.rihab_bahri.name
+}
+
+resource "snowflake_grant_account_role" "grant_poc_viseo_role_mnoiret" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_account_role.role.name
+  user_name = snowflake_user.matthieu_noiret.name
+}
+
+resource "snowflake_grant_account_role" "grant_poc_viseo_role_asnoussi" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_account_role.role.name
+  user_name = snowflake_user.amine_snoussi.name
 }
