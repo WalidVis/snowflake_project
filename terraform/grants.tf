49fc8d4bb4fd23302deea260e4c4c7a743e63bc3
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-11 11:38:32 +0100

    comment grants.tf for test

--- terraform/grants.tf
+++ terraform/grants.tf
@@ -1,5 +1,6 @@
-resource "snowflake_grant_account_role" "grant_poc_viseo_role" {
+/*resource "snowflake_grant_account_role" "grant_poc_viseo_role" {
   provider  = snowflake.security_admin
   role_name = snowflake_account_role.role.name
   user_name = snowflake_user.saif_bouaziz.name
 }
+*/
