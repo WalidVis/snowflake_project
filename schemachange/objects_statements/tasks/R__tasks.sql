9209a7679bc757a51482284298fd30e3d3b0c714
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-11 14:26:02 +0100

    Delete databse namespace in tasks

--- schemachange/objects_statements/tasks/R__tasks.sql
+++ schemachange/objects_statements/tasks/R__tasks.sql
@@ -2,19 +2,19 @@ create or replace task BRONZE_LAYER."ingest_PRC_CAMPAIGN_MARKET_csv"
 	warehouse=COMPUTE_WH
 	schedule='USING CRON 1 1 * * * Europe/Paris'
 	config='{
-  "params": "''{ \\"src_schema\\": \\"DEV_POC_VISEO_DB.raw_layer\\",
-  \\"target_table\\": \\"DEV_POC_VISEO_DB.bronze_layer.PRC_CAMPAIGN_MARKET_BRZ\\",
-  \\"stage_name\\": \\"@DEV_POC_VISEO_DB.raw_layer.landing_internal_stage\\",
+  "params": "''{ \\"src_schema\\": \\"raw_layer\\",
+  \\"target_table\\": \\"bronze_layer.PRC_CAMPAIGN_MARKET_BRZ\\",
+  \\"stage_name\\": \\"@raw_layer.landing_internal_stage\\",
   \\"stage_path_suffix\\" :\\"/PRC_CAMPAIGN_MARKET/\\",
   \\"pattern_file_name\\": \\".*.csv\\",
-  \\"file_format\\" : \\"DEV_POC_VISEO_DB.bronze_layer.csv_file_format\\",
+  \\"file_format\\" : \\"bronze_layer.csv_file_format\\",
    \\"on_error\\": \\"SKIP_FILE\\",
-   \\"external_stage_root_path\\": \\"@DEV_POC_VISEO_DB.RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"
+   \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"
 }'
 as
     BEGIN
         LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string;
-	    EXECUTE NOTEBOOK "DEV_POC_VISEO_DB"."BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS);
+	    EXECUTE NOTEBOOK "BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS);
     END;
 
     ---------------------------------------------------
@@ -24,16 +24,16 @@ as
     create or replace task BRONZE_LAYER."ingest_PRC_CUSTOMER_ERP_PRICING_MARKET_json"
 	warehouse=COMPUTE_WH
 	schedule='USING CRON 0 5 * * * Europe/Paris'
-	config='{"params":"''{ \\"src_schema\\": \\"DEV_POC_VISEO_DB.raw_layer\\",
-	 \\"target_table\\": \\"DEV_POC_VISEO_DB.BRONZE_LAYER.PRC_CUSTOMER_ERP_PRICING_MARKET_BRZ\\",
-	   \\"stage_name\\": \\"@DEV_POC_VISEO_DB.raw_layer.landing_internal_stage\\",
+	config='{"params":"''{ \\"src_schema\\": \\"raw_layer\\",
+	 \\"target_table\\": \\"BRONZE_LAYER.PRC_CUSTOMER_ERP_PRICING_MARKET_BRZ\\",
+	   \\"stage_name\\": \\"@raw_layer.landing_internal_stage\\",
 	    \\"stage_path_suffix\\" :\\"/PRC_CUSTOMER_ERP_PRICING_MARKET/\\",
 	     \\"pattern_file_name\\": \\".*.json\\",
-	      \\"file_format\\" : \\"DEV_POC_VISEO_DB.bronze_layer.json_file_format\\",
+	      \\"file_format\\" : \\"bronze_layer.json_file_format\\",
 	      \\"on_error\\": \\"CONTINUE\\",
-	      \\"external_stage_root_path\\": \\"@DEV_POC_VISEO_DB.RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"}'
+	      \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"}'
 as
 	BEGIN
 	    LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string;
-	    EXECUTE NOTEBOOK "DEV_POC_VISEO_DB"."BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS);
+	    EXECUTE NOTEBOOK "BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS);
 	END;
