3c82829fd0b7629be1225c8dcc27affb4a98734b
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-12 10:07:52 +0100

    Task with EXECUTE IMMEDIATE OK

--- schemachange/objects_statements/tasks/R__tasks.sql
+++ schemachange/objects_statements/tasks/R__tasks.sql
@@ -5,7 +5,7 @@ create or replace task BRONZE_LAYER."ingest_PRC_BENCHMARK_LEVEL_csv"
   "params": "''{ \\"src_schema\\": \\"raw_layer\\",  \\"target_table\\": \\"bronze_layer.PRC_BENCHMARK_LEVEL\\",  \\"stage_name\\": \\"@raw_layer.landing_internal_stage\\",  \\"stage_path_suffix\\" :\\"/PRC_BENCHMARK_LEVEL/\\",  \\"pattern_file_name\\": \\".*.csv\\",  \\"file_format\\" : \\"bronze_layer.csv_file_format\\", \\"on_error\\": \\"SKIP_FILE\\", \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"
 }'
 as
-EXECUTE $$ IMMEDIATE  BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string; EXECUTE NOTEBOOK "BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS); END;$$;
+EXECUTE IMMEDIATE $$  BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string; EXECUTE NOTEBOOK "BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS); END;$$;
 
     ---------------------------------------------------
     --------------------------------------------------------------------------
@@ -23,7 +23,9 @@ EXECUTE $$ IMMEDIATE  BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('p
 	      \\"on_error\\": \\"CONTINUE\\",
 	      \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"}'
 as
+EXECUTE IMMEDIATE $$
 	BEGIN
 	    LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string;
 	    EXECUTE NOTEBOOK "BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS);
 	END;
+	$$;
