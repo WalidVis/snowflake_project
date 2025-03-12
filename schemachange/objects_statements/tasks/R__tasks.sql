2dd49eb2b982140d2fb2ee96a40200ab739af184
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-12 10:22:56 +0100

    Remove LR in the json params

--- schemachange/objects_statements/tasks/R__tasks.sql
+++ schemachange/objects_statements/tasks/R__tasks.sql
@@ -14,14 +14,7 @@ EXECUTE IMMEDIATE $$  BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('p
     create or replace task BRONZE_LAYER."ingest_PRC_CUSTOMER_ERP_PRICING_MARKET_json"
 	warehouse=COMPUTE_WH
 	schedule='USING CRON 0 5 * * * Europe/Paris'
-	config='{"params":"''{ \\"src_schema\\": \\"raw_layer\\",
-	 \\"target_table\\": \\"BRONZE_LAYER.PRC_CUSTOMER_ERP_PRICING_MARKET_BRZ\\",
-	   \\"stage_name\\": \\"@raw_layer.landing_internal_stage\\",
-	    \\"stage_path_suffix\\" :\\"/PRC_CUSTOMER_ERP_PRICING_MARKET/\\",
-	     \\"pattern_file_name\\": \\".*.json\\",
-	      \\"file_format\\" : \\"bronze_layer.json_file_format\\",
-	      \\"on_error\\": \\"CONTINUE\\",
-	      \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"}'
+	config='{"params":"''{ \\"src_schema\\": \\"raw_layer\\", \\"target_table\\": \\"BRONZE_LAYER.PRC_CUSTOMER_ERP_PRICING_MARKET_BRZ\\", \\"stage_name\\": \\"@raw_layer.landing_internal_stage\\", \\"stage_path_suffix\\" :\\"/PRC_CUSTOMER_ERP_PRICING_MARKET/\\", \\"pattern_file_name\\": \\".*.json\\", \\"file_format\\" : \\"bronze_layer.json_file_format\\", \\"on_error\\": \\"CONTINUE\\", \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"}'
 as
 EXECUTE IMMEDIATE $$
 	BEGIN
