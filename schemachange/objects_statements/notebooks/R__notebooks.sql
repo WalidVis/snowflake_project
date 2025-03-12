ac9e3695a6fa06ab3bb2e45e1560aabc3106ede9
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-12 12:10:10 +0100

    Change notebook python file place and croorect create notebook statement

--- schemachange/objects_statements/notebooks/R__notebooks.sql
+++ schemachange/objects_statements/notebooks/R__notebooks.sql
@@ -1,7 +1,8 @@
-PUT file://../schemachange/notebooks/INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb @~/staged_notebooks;
+--PUT the notebook files in the user stage
+PUT file://notebooks/INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb @~/staged_notebooks;
 
 
-CREATEOR OR REPLACE NOTEBOOK INGEST_RAW_FILES_INTO_BRONZE_LAYER
+CREATE OR REPLACE NOTEBOOK INGEST_RAW_FILES_INTO_BRONZE_LAYER
   FROM '@~/staged_notebooks' 
   MAIN_FILE = 'INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb' 
    COMMENT = 'Notebook to ingest file data into bronze layer tables';
