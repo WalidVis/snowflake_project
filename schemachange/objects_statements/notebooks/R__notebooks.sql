e8a33264c44fea72596051db4c5971278e15619c
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-12 14:03:15 +0100

    Correct notebook sql and add CREATE notebook privilege to the ROLE

--- schemachange/objects_statements/notebooks/R__notebooks.sql
+++ schemachange/objects_statements/notebooks/R__notebooks.sql
@@ -1,8 +1,9 @@
 --PUT the notebook files in the user stage
-PUT file://notebooks/INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb @~/staged_notebooks;
+-- Working directory is schemachange directory (set in the CI and CD schemachange step)
+PUT file://objects_statements/notebooks/INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb @~/staged_notebooks;
 
 
-CREATE OR REPLACE NOTEBOOK INGEST_RAW_FILES_INTO_BRONZE_LAYER
+CREATE OR REPLACE NOTEBOOK BRONZE_LAYER.INGEST_RAW_FILES_INTO_BRONZE_LAYER
   FROM '@~/staged_notebooks' 
   MAIN_FILE = 'INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb' 
    COMMENT = 'Notebook to ingest file data into bronze layer tables';
