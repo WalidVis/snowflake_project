a08afbd5614a815c5764c93ccd5d34c2706ce851
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-17 15:08:36 +0100

    delete comment from task.sql

--- schemachange/objects_statements/tasks/R__tasks.sql
+++ schemachange/objects_statements/tasks/R__tasks.sql
@@ -24,7 +24,7 @@ EXECUTE IMMEDIATE $$  BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('p
 --------------------------------------------------------------------------
 ---------------------------------------------------
 
-    create or replace task BRONZE_LAYER."ingest_PRC_CUSTOMER_ERP_PRICING_MARKET_json"
+create or replace task BRONZE_LAYER."ingest_PRC_CUSTOMER_ERP_PRICING_MARKET_json"
 	warehouse=COMPUTE_WH
 	schedule='USING CRON 0 5 * * * Europe/Paris'
 	--  [ TASK_AUTO_RETRY_ATTEMPTS = <num> ] 
@@ -42,7 +42,7 @@ EXECUTE IMMEDIATE $$
 ---------------------------------------------------
 
 
-	create or replace task BRONZE_LAYER."ingest_PRC_BENCHMARK_csv"
+create or replace task BRONZE_LAYER."ingest_PRC_BENCHMARK_csv"
 	warehouse=COMPUTE_WH
 	schedule='USING CRON 0 5 * * * Europe/Paris'
 	--  [ TASK_AUTO_RETRY_ATTEMPTS = <num> ]
@@ -56,7 +56,7 @@ EXECUTE IMMEDIATE $$
 ---------------------------------------------------
 
 
-	create or replace task BRONZE_LAYER."ingest_PRC_CAMPAIGN_MARKET_csv"
+create or replace task BRONZE_LAYER."ingest_PRC_CAMPAIGN_MARKET_csv"
 	warehouse=COMPUTE_WH
 	--  [ TASK_AUTO_RETRY_ATTEMPTS = <num> ]
 	schedule='USING CRON 1 1 * * * Europe/Paris'
@@ -64,14 +64,3 @@ EXECUTE IMMEDIATE $$
   "params": "''{ \\"src_schema\\": \\"raw_layer\\",  \\"target_table\\": \\"bronze_layer.PRC_CAMPAIGN_MARKET_BRZ\\",  \\"stage_name\\": \\"@raw_layer.landing_internal_stage\\",  \\"stage_path_suffix\\" :\\"/PRC_CAMPAIGN_MARKET/\\",  \\"pattern_file_name\\": \\".*.csv\\",  \\"file_format\\" : \\"bronze_layer.csv_file_format\\", \\"on_error\\": \\"SKIP_FILE\\", \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\"}''"
 }'
 	as EXECUTE IMMEDIATE $$ BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string; EXECUTE NOTEBOOK "BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS); END; $$;
-
----------------------------------------------------
---------------------------------------------------------------------------
----------------------------------------------------
-
--- STREAMING PIPELINE TASKS
-
----------------------------------------------------
---------------------------------------------------------------------------
----------------------------------------------------
-
