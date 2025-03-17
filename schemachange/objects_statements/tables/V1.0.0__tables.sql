7eddc6c7c66e6aa571fe37fc18c7f7fd17b0b302
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-17 11:06:26 +0100

    Correct layer

--- schemachange/objects_statements/tables/V1.0.0__tables.sql
+++ schemachange/objects_statements/tables/V1.0.0__tables.sql
@@ -165,7 +165,7 @@ create or replace table MONITORING_LAYER.MONITORING_INGEST
 
  -------------------------------------------- Create File formats ---------------------
 
-create or replace file format bronze_layer.csv_file_format 
+create or replace file format BRONZE_LAYER.csv_file_format 
     TYPE = CSV 
     PARSE_HEADER = TRUE -- Use first row for column names
     TRIM_SPACE = TRUE -- deletes spaces outter string 
@@ -174,7 +174,7 @@ create or replace file format bronze_layer.csv_file_format
     ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE -- Because loading CSV with INCLUDE_METADATA
 ;
 
-create or replace file format bronze_layer.json_file_format 
+create or replace file format BRONZE_LAYER.json_file_format 
     TYPE = JSON 
     -- NULL_IF = ('\N', 'NULL', 'NUL', '') // To convert to SQL NULL 
     TRIM_SPACE = TRUE // deletes spaces outter white spaces 
