033a094b4a52b18b1a3f5bcb55cde3fefc7c1dcb
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-24 00:22:11 +0100

    Add the warehouse to task benchmark

--- schemachange/objects_statements/tasks/R__tasks.sql
+++ schemachange/objects_statements/tasks/R__tasks.sql
@@ -43,6 +43,7 @@ EXECUTE IMMEDIATE $$
 
 
 create or replace task BRONZE_LAYER."ingest_prc_benchmark_csv"
+	warehouse=TEST_WH
 	schedule='USING CRON 1 * * * * Europe/Paris'
 	config='{"params":"''{ \\"src_schema\\" : \\"raw_layer\\", \\"external_stage_root_path\\": \\"@RAW_LAYER.EXTERNAL_AZUR_STAGE/Files\\", \\"stage_name\\": \\"@raw_layer.landing_internal_stage\\",  \\"stage_path_suffix\\" :\\"/PRC_BENCHMARK/\\", \\"pattern_file_name\\": \\".*.csv\\",  \\"on_error\\": \\"CONTINUE\\", \\"file_format\\" : \\"bronze_layer.csv_file_format\\",  \\"bronze_table\\": \\"bronze_layer.PRC_BENCHMARK_BRZ\\",  \\"silver_table\\" :\\"silver_layer.TEST_DIM_PRC_BENCHMARK_SLV\\", \\"silver_technicalKey_name\\" : \\"PricingBenchmarkPrcIntKey\\", \\"silver_functionalKey_name\\" : \\"PRICINGBENCHMARKPRCKEY\\", \\"silver_ruleTechnicalKey\\": \\"HASH(CONCAT(COALESCE(APUKCODE, ''\\\\''N/A\\\\''''), ''\\\\''_\\\\'''', COALESCE(ANABENCH2, ''\\\\''N/A\\\\'''')))\\", \\"silver_ruleFunctionalKey\\" : \\"CONCAT(COALESCE(APUKCODE, ''\\\\''N/A\\\\''''), ''\\\\''_\\\\'''', COALESCE(ANABENCH2, ''\\\\''N/A\\\\''''))\\"}''"}'
 	as EXECUTE IMMEDIATE $$ BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string; EXECUTE NOTEBOOK "TEST_POC_VISEO_DB"."BRONZE_LAYER"."INGEST_RAW_FILES_INTO_BRONZE_LAYER"(:PARAMS); END;$$;
@@ -77,8 +78,9 @@ create or replace task BRONZE_LAYER."ingest_prc_geography_json"
 -------------------------------- SILVER LAYER INGEST TASKS (MUST BE CREATED IN BRONZE LAYER)
 ---------------------------------------------------
 
-create or replace task BRONZE_LAYER.ingest_prc_benchmark_silver
-	after TEST_POC_VISEO_DB.BRONZE_LAYER."ingest_prc_benchmark_csv"
+create or replace task BRONZE_LAYER."ingest_prc_benchmark_silver"
+	warehouse=TEST_WH
+	after BRONZE_LAYER."ingest_prc_benchmark_csv"
 	as EXECUTE IMMEDIATE $$ 
 	BEGIN LET PARAMS STRING := SYSTEM$GET_TASK_GRAPH_CONFIG('params')::string; EXECUTE NOTEBOOK "SILVER_LAYER"."INGEST_INTO_SILVER_LAYER"(:PARAMS); END;$$;
 
