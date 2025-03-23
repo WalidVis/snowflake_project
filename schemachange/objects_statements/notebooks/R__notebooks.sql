bd7ef91696a8888bba4396414eab0cacb1f4534a
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-23 23:54:47 +0100

    Add removing notebook staged

--- schemachange/objects_statements/notebooks/R__notebooks.sql
+++ schemachange/objects_statements/notebooks/R__notebooks.sql
@@ -7,11 +7,14 @@ PUT file://objects_statements/notebooks/INGEST_INTO_SILVER_LAYER.ipynb @raw_laye
 AUTO_COMPRESS = FALSE;
 
 CREATE OR REPLACE NOTEBOOK BRONZE_LAYER.INGEST_RAW_FILES_INTO_BRONZE_LAYER
-  FROM '@raw_layer.landing_internal_stage/staged_notebooks' 
-  MAIN_FILE = 'INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb' 
-   COMMENT = 'Notebook to ingest file data into bronze layer tables';
+FROM '@raw_layer.landing_internal_stage/staged_notebooks' 
+ MAIN_FILE = 'INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb' 
+COMMENT = 'Notebook to ingest file data into bronze layer tables';
 
-   CREATE OR REPLACE NOTEBOOK SILVER_LAYER.INGEST_INTO_SILVER_LAYER
-  FROM '@raw_layer.landing_internal_stage/staged_notebooks' 
-  MAIN_FILE = 'INGEST_INTO_SILVER_LAYER.ipynb' 
-   COMMENT = 'Notebook to ingest data from bronze tables to silver layer tables';
+CREATE OR REPLACE NOTEBOOK SILVER_LAYER.INGEST_INTO_SILVER_LAYER
+FROM '@raw_layer.landing_internal_stage/staged_notebooks' 
+MAIN_FILE = 'INGEST_INTO_SILVER_LAYER.ipynb' 
+  COMMENT = 'Notebook to ingest data from bronze tables to silver layer tables';
+
+
+REMOVE @raw_layer.landing_internal_stage/staged_notebooks
