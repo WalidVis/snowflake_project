466a26dcc176b593539b5b53f180a9ae7ac7e9b3
Walid <mouloud-walid.cheriet@viseo.com>
2025-03-23 22:51:17 +0100

    Correct put file notebook

--- schemachange/objects_statements/notebooks/R__notebooks.sql
+++ schemachange/objects_statements/notebooks/R__notebooks.sql
@@ -1,15 +1,17 @@
 --PUT the notebook files in the landing stage
 -- Working directory is schemachange directory (set in the CI and CD schemachange step)
-PUT file://objects_statements/notebooks/? @raw_layer.landing_internal_stage/staged_notebooks
+PUT file://objects_statements/notebooks/INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb @raw_layer.landing_internal_stage/staged_notebooks
 AUTO_COMPRESS = FALSE;
 
+PUT file://objects_statements/notebooks/INGEST_INTO_SILVER_LAYER.ipynb @raw_layer.landing_internal_stage/staged_notebooks
+AUTO_COMPRESS = FALSE;
 
 CREATE OR REPLACE NOTEBOOK BRONZE_LAYER.INGEST_RAW_FILES_INTO_BRONZE_LAYER
   FROM '@raw_layer.landing_internal_stage/staged_notebooks' 
   MAIN_FILE = 'INGEST_RAW_FILES_INTO_BRONZE_LAYER.ipynb' 
    COMMENT = 'Notebook to ingest file data into bronze layer tables';
 
-CREATE OR REPLACE NOTEBOOK SILVER_LAYER.INGEST_INTO_SILVER_LAYER
+   CREATE OR REPLACE NOTEBOOK SILVER_LAYER.INGEST_INTO_SILVER_LAYER
   FROM '@raw_layer.landing_internal_stage/staged_notebooks' 
   MAIN_FILE = 'INGEST_INTO_SILVER_LAYER.ipynb' 
-   COMMENT = 'Notebook to ingest data from bronze tables to silver tables';
+   COMMENT = 'Notebook to ingest data from bronze tables to silver layer tables';
